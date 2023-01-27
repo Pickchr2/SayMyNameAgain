bbba108b2cb36d4f6d4a77e917fd0ab71f571cf2
Chris Pickens <Cpickens1270@gmail.com>
2023-01-27 14:12:39 -0700

    Set Heading and preffered options

--- SayMyNameAgain/Program.vb
+++ SayMyNameAgain/Program.vb
@@ -1,7 +1,17 @@
+'Chris Pickens
+'RCET0265
+'Spring 2023
+'Say My Name Again
+'https://github.com/Pickchr2/SayMyNameAgain.git
+
+Option Explicit On
+Option Strict On
+Option Compare Text
+
 Imports System
 
 Module Program
     Sub Main(args As String())
-        Console.WriteLine("Hello World!")
+
     End Sub
 End Module
